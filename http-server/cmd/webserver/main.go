f2fdc402356d44557422dc12ccdb1c4a1e99f7b3
Ichi <Ichi@Ichi.com>
2025-01-01 14:05:14 -0800

    sef

--- http-server/cmd/webserver/main.go
+++ http-server/cmd/webserver/main.go
@@ -4,6 +4,9 @@ import (
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/IchiThe2nd/TheApp/http-server/poker"
+	// "github.com/IchiThe2nd/TheApp/http-server/poker"
 )
 
 const dbFileName = "game.db.json"
@@ -16,11 +19,11 @@ func main() {
 		log.Fatalf("problem opening %s %v", dbFileName, err)
 	}
 
-	store, err := NewFileSystemPlayerStore(db)
+	store, err := poker.NewFileSystemPlayerStore(db)
 	if err != nil {
 		log.Fatalf("problem creating filesystem palyer stopre , %v", err)
 	}
-	server := NewPlayerServer(store)
+	server := poker.NewPlayerServer(store)
 
 	if err := http.ListenAndServe(":5000", server); err != nil {
 		log.Fatalf("could not list enm on portt 5000 %v", err)
